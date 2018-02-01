a7967831632e7bb094e3a8f6a9350b8259de3343
cswhite2000 <18whitechristop@gmail.com>
2018-01-31 18:10:09 -0800

    Add async chunk load API from Paper

--- snapshot/CraftBukkit/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
+++ snapshot/CraftBukkit/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
@@ -185,6 +185,26 @@ public class CraftWorld implements World {
         }
     }
 
+    // Paper start - Async chunk load API
+    public void getChunkAtAsync(final int x, final int z, final ChunkLoadCallback callback) {
+        final ChunkProviderServer cps = this.world.getChunkProviderServer();
+        cps.getChunkAt(x, z, new Runnable() {
+            @Override
+            public void run() {
+                callback.onLoad(cps.getChunkAt(x, z).bukkitChunk);
+            }
+        });
+    }
+
+    public void getChunkAtAsync(Block block, ChunkLoadCallback callback) {
+        getChunkAtAsync(block.getX() >> 4, block.getZ() >> 4, callback);
+    }
+
+    public void getChunkAtAsync(Location location, ChunkLoadCallback callback) {
+        getChunkAtAsync(location.getBlockX() >> 4, location.getBlockZ() >> 4, callback);
+    }
+    // Paper end
+
     public Chunk getChunkAt(int x, int z) {
         return this.world.getChunkProviderServer().getChunkAt(x, z).bukkitChunk;
     }
